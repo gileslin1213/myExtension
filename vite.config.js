207eed58d3c76aad9f14eb153e94560ea047b18a
gileslin <gileslin1213@gmail.com>
2022-07-05 19:38:57 +0800

    import element-plus darkmode && setting autoimport component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import Vue from '@vitejs/plugin-vue'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+    }),
+    Components({
+      dirs: ["src/components"],
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+    }),
+  ],
+});

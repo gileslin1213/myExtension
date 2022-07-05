14538102bdc08406cb419b09e4f03ceedde5ec25
gileslin <gileslin1213@gmail.com>
2022-07-06 02:03:56 +0800

    remove unuse file && formate config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
@@ -9,16 +9,12 @@ export default defineConfig({
   plugins: [
     Vue(),
     AutoImport({
-      imports: ['vue', '@vueuse/core'],
-      resolvers: [
-        ElementPlusResolver(),
-      ],
+      imports: ["vue", "@vueuse/core"],
+      resolvers: [ElementPlusResolver()],
     }),
     Components({
       dirs: ["src/components"],
-      resolvers: [
-        ElementPlusResolver(),
-      ],
+      resolvers: [ElementPlusResolver()],
     }),
   ],
 });
